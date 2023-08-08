e724de44c31a8cf4642dc3007d93f0d2843efafe
angelolustosa <angelolustosa@gmail.com>
2023-08-07 21:56:47 -0300

    Inicio do server com model e controller

---
+++ server.js
@@ -0,0 +1,14 @@
+
+import './src/db/dbConnect.js'
+import app from './src/app.js'
+
+const HOST = 'localhost'
+const PORT = 5000
+
+app.get('/', (req, res) => {
+  res.send('Servidor no Ar!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port http://${HOST}:${PORT}`)
+})
